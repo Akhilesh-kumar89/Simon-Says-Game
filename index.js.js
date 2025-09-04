a8b520291bd493fd6295428b22a4ff251016d9ae
Akhilesh Kumar <kushwaha345@gmail.com>
2025-09-04 19:03:05 +0530

    Initial commit

---
+++ index.js.js
@@ -0,0 +1,110 @@
+let gameSeq=[];
+let userSeq=[];
+
+let btns=["yellow","red","purple","green"];
+
+let started = false;
+let level=0;
+ 
+let h2=document.querySelector("h2");
+
+
+document.addEventListener("keypress", function () {
+   // console.log("Game started");
+   if(started  == false){
+    console.log("game is started");
+    started=true;
+
+    levelUp();
+   }
+
+});
+
+function gameflash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function (){
+        btn.classList.remove("flash");
+    },250);
+}
+
+function userflash(btn) {
+    btn.classList.add("userflash");
+    setTimeout(function (){
+        btn.classList.remove("userflash");
+    },250);
+}
+
+function levelUp(){
+    userSeq = [];
+    level++;
+    h2.innerText=`Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() *3);
+    let randcolor= btns[randIdx];
+    let randbtn= document.querySelector(`.${randcolor}`);
+    // console.log(randIdx);
+    // console.log(randcolor);
+    // console.log(randbtn);
+    gameSeq.push(randcolor);
+    console.log(gameSeq);
+
+
+
+    gameflash(randbtn);
+
+}
+
+
+function checkAns(idx){
+   // console.log("curr level:",level);
+   //let idx = level-1;
+
+   if (userSeq[idx] === gameSeq[idx]) {
+    if (userSeq.length == gameSeq.length) {
+        setTimeout(levelUp,1000);
+    }
+    
+   } else{
+    h2.innerHTML=`Game over !! your score was <b>${level} </b> <br>press any key to start`;
+    document.querySelector("body").style.backgroundColor="red";
+    setTimeout( function(){
+        document.querySelector("body").style.backgroundColor="white";
+    },150);
+    reset()
+   }
+
+}
+function btnPress (){
+    let btn=this;
+    console.log(this);
+    userflash(btn);
+
+    userColor= btn.getAttribute("id");
+    userSeq.push(userColor);
+    checkAns(userSeq.length-1);
+}
+
+
+
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns) {
+    btn.addEventListener("click",btnPress);
+}
+
+function reset(){
+    started = false;
+    gameSeq =[];
+    userSeq = [];
+    level = 0;
+
+}
+
+  let name=prompt("Enter the Username:");
+// console.log(` Hii, ${name} Welcome to Simon Say Game.`);
+
+ let h22 = document.getElementById("demo").innerHTML=`Hii, ${name} Welcom to Synam say Game.`;
+ document.getElementById("demo").style.backgroundColor="pink";
+ document.getElementById("demo").style.color="chocolate";
+ document.getElementById("demo").style.fontStyle="italic";
+
+ alert(`thanks," ${name} " Start the game`);
